9ccff7b0676146bee37e38853992cf5a8c9c3f65
Ryoichi Gokan <nargok@gmail.com>
2018-07-20 08:02:43 +0900

    implement user create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :password, length: { minimum: 8 }, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
 end
