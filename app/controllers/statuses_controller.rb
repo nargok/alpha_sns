804d2c00f0fe93260e15996706e6f001fb0690d5
Ryoichi Gokan <nargok@gmail.com>
2018-07-22 12:20:20 +0900

    implement status create functionality

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,23 @@
 class StatusesController < ApplicationController
   def index
-    @statuses = Status.all
+    @statuses = Status.all.order(created_at: 'desc')
+    @status = Status.new
+  end
+
+  def create
+    @status = Status.new(status_params)
+    @status.user_id = current_user.id
+
+    if @status.save
+      redirect_to root_path
+    else
+      # TODO flashメッセージを実装する
+    end
+  end
+
+  private
+
+  def status_params
+    params.require(:status).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   resources :users
   resource :session, only: [:new, :create, :destroy]
+  resources :statuses, only: [:index, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
